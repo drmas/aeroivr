d8f552ba14de26342b2733d04b8900f89c5aab70
AndriyPt <AndriyPt@777c3621-373b-0410-8042-373624c80bc3>
2007-11-25 21:32:53 +0000

    Added projects with appropriate packages. Projects were created by NetBeans 5.5.1 IDE

---
+++ trunk/AppServer/src/org/aeroivr/appserver/admin/application.java
@@ -0,0 +1,25 @@
+/*
+ * application.java
+ *
+ * Created on 25 листопада 2007, 18:12
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.aeroivr.appserver.admin;
+
+/**
+ *
+ * @author Andriy
+ */
+public class application {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
