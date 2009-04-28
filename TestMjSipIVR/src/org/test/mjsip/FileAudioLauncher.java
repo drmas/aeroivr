cf72ffa7c7941707b8bfecf8216ac6ea75694fdb
AndriyPt <AndriyPt@777c3621-373b-0410-8042-373624c80bc3>
2009-04-28 21:54:15 +0000

    Added input file stream with multiple files support.

--- TestMjSipIVR/src/org/test/mjsip/FileAudioLauncher.java
+++ TestMjSipIVR/src/org/test/mjsip/FileAudioLauncher.java
@@ -1,5 +1,7 @@
 package org.test.mjsip;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramSocket;
 
 import javax.sound.sampled.AudioFormat;
@@ -16,7 +18,7 @@ import org.zoolu.tools.Log;
 import org.zoolu.tools.LogLevel;
 
 public class FileAudioLauncher  implements MediaLauncher {
-	
+
 	   /** Event logger. */
 	   Log log=null;
 
@@ -31,10 +33,10 @@ public class FileAudioLauncher  implements MediaLauncher {
 	   /** Frame rate [frames per second] */
 	   int frame_rate=16; //=sample_rate/(frame_size/sample_size);
 	   AudioFormat.Encoding codec=AudioFormat.Encoding.ULAW;
-	   boolean signed=false; 
+	   boolean signed=false;
 	   boolean big_endian=false;
 
-	   //String filename="audio.wav"; 
+	   //String filename="audio.wav";
 
 	   /** Test tone */
 	   public static final String TONE="TONE";
@@ -46,17 +48,17 @@ public class FileAudioLauncher  implements MediaLauncher {
 
 	   /** Runtime media process */
 	   Process media_process=null;
-	   
-	   int dir; // duplex= 0, recv-only= -1, send-only= +1; 
+
+	   int dir; // duplex= 0, recv-only= -1, send-only= +1;
 
 	   DatagramSocket socket=null;
 	   RtpStreamSender sender=null;
 	   RtpStreamReceiver receiver=null;
-	   AudioInput audio_input=null;
+	   MultipleFilesInputStream audio_input_stream=null;
 	   AudioOutput audio_output=null;
-	   
+
 	   ToneInputStream tone=null;
-	   
+
 	   private void init(int local_port, String remote_addr, int remote_port, int direction, Log logger)
 	   {
 		   log=logger;
@@ -68,9 +70,9 @@ public class FileAudioLauncher  implements MediaLauncher {
 		         {  printLog("new audio sender to "+remote_addr+":"+remote_port,LogLevel.MEDIUM);
 		            //audio_input=new AudioInput();
 		            AudioFormat format=new AudioFormat(codec,sample_rate,8*sample_size,1,sample_size,sample_rate,big_endian);
-		            audio_input=new AudioInput(format);
-		            //sender=new RtpStreamSender(audio_input.getInputStream(),false,payload_type,frame_rate,frame_size,socket,remote_addr,remote_port);
-		            sender=new RtpStreamSender(audio_input.getInputStream(),true,payload_type,frame_rate,frame_size,socket,remote_addr,remote_port);
+		            //sender=new RtpStreamSender(audio_input_stream,false,payload_type,frame_rate,frame_size,socket,remote_addr,remote_port);
+		            audio_input_stream = new MultipleFilesInputStream();
+		            sender=new RtpStreamSender(audio_input_stream,true,payload_type,frame_rate,frame_size,socket,remote_addr,remote_port);
 		            sender.setSyncAdj(2);
 		         }
 		         // receiver
@@ -82,7 +84,7 @@ public class FileAudioLauncher  implements MediaLauncher {
 		            receiver=new RtpStreamReceiver(audio_output.getOuputStream(),socket);
 		         }
 		      }
-		      catch (Exception e) {  printException(e,LogLevel.HIGH);  }		   
+		      catch (Exception e) {  printException(e,LogLevel.HIGH);  }
 	   }
 
 	   /** Constructs the audio launcher */
@@ -90,8 +92,8 @@ public class FileAudioLauncher  implements MediaLauncher {
 	   {
 		   init(local_port, remote_addr, remote_port, direction, logger);
 	   }
-	   
-	   public FileAudioLauncher(int local_port, String remote_addr, int remote_port, int direction, int sample_rate, 
+
+	   public FileAudioLauncher(int local_port, String remote_addr, int remote_port, int direction, int sample_rate,
 			   int sample_size, int frame_size, Log logger)
 	   {
 		   this.sample_rate = sample_rate;
@@ -99,7 +101,13 @@ public class FileAudioLauncher  implements MediaLauncher {
 		   this.frame_size = frame_size;
 		   init(local_port, remote_addr, remote_port, direction, logger);
 	   }
-	   
+
+	   public void addFileToPlay(String file_name)
+	   {
+		   if (null != audio_input_stream) {
+			   audio_input_stream.addFileToPlay(file_name);
+		   }
+	   }
 
 	   /** Starts media application */
 	   public boolean startMedia()
@@ -108,32 +116,37 @@ public class FileAudioLauncher  implements MediaLauncher {
 	      if (sender!=null)
 	      {  printLog("start sending",LogLevel.LOW);
 	         sender.start();
-	         if (audio_input!=null) audio_input.play();
 	      }
 	      if (receiver!=null)
 	      {  printLog("start receiving",LogLevel.LOW);
 	         receiver.start();
 	         if (audio_output!=null) audio_output.play();
 	      }
-	      
-	      return true;      
+
+	      return true;
 	   }
 
 
 	   /** Stops media application */
 	   public boolean stopMedia()
-	   {  printLog("halting java audio..",LogLevel.HIGH);    
+	   {  printLog("halting java audio..",LogLevel.HIGH);
 	      if (sender!=null)
 	      {  sender.halt(); sender=null;
 	         printLog("sender halted",LogLevel.LOW);
-	      }      
-	      if (audio_input!=null)
-	      {  audio_input.stop(); audio_output=null;
-	      }      
+	      }
+	      if (null != audio_input_stream) {
+	    	  try {
+				audio_input_stream.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	  audio_input_stream = null;
+	      }
 	      if (receiver!=null)
 	      {  receiver.halt(); receiver=null;
 	         printLog("receiver halted",LogLevel.LOW);
-	      }      
+	      }
 	      if (audio_output!=null)
 	      {  audio_output.stop(); audio_output=null;
 	      }
@@ -156,8 +169,8 @@ public class FileAudioLauncher  implements MediaLauncher {
 
 	   /** Adds a new string to the default Log */
 	   private void printLog(String str, int level)
-	   {  if (log!=null) log.println("AudioLauncher: "+str,level+SipStack.LOG_LEVEL_UA);  
-	      if (level<=LogLevel.HIGH) System.out.println("AudioLauncher: "+str);
+	   {  if (log!=null) log.println("FileAudioLauncher: "+str,level+SipStack.LOG_LEVEL_UA);
+	      if (level<=LogLevel.HIGH) System.out.println("FileAudioLauncher: "+str);
 	   }
 
 	   /** Adds the Exception message to the default Log */
